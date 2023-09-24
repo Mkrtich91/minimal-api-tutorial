0c385d20c8d657f16a08691ac38d228051258b44
Mkrtich Kazanchian <mkogeo@gmail.com>
2023-09-24 12:50:31 +0400

    solved ver2

---
+++ TodoAppApi/Todo.cs
@@ -0,0 +1,8 @@
+namespace TodoAppApi;
+public class Todo
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+    public string? Secret { get; set; }
+}
